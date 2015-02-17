7527ce671ceffab94041c9648a8b205fa630601a
John Chodera (MSKCC) <choderaj@mskcc.org>
2015-02-17 11:51:24 +0100

    Checking in basic Python project skeleton

---
+++ bhmm/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/local/bin/env python
+
+"""
+BHMM: A toolkit for Bayesian hidden Markov model analysis of single-molecule trajectories.
+
+"""
+
+# Define global version.
+import version
+__version__ = version.version
---
+++ bhmm/tests/test_bhmm.py
@@ -0,0 +1,10 @@
+#!/usr/local/bin/env python
+
+"""
+Test BHMM using simple analytical models.
+
+"""
+
+def test_bhmm():
+    # TODO: Build tests.
+    return
