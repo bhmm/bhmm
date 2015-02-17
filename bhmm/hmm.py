Fixes to stationary distribution computation and tests.

bhmm/hmm.py
@@ -15,7 +15,7 @@ class HMM(object):
     --------
 
     >>> nstates = 2
-    >>> Tij = np.array([[0.5, 0.5], [0.5, 0.5]])
+    >>> Tij = np.array([[0.8, 0.2], [0.5, 0.5]])
     >>> states = [ {'mu' : -1, 'sigma' : 1}, {'mu' : +1, 'sigma' : 1} ]
     >>> model = HMM(nstates, Tij, states)
 
@@ -46,7 +46,7 @@ class HMM(object):
         return np.log(self.Pi)
 
     @classmethod
-    def _compute_stationary_probabilities(cls, Tij, tol=1e-5, maxits=None):
+    def _compute_stationary_probabilities(cls, Tij, tol=1e-5, maxits=None, method='arpack'):
         """Compute the stationary probabilities for a given transition matrix.
 
         Parameters
@@ -55,8 +55,10 @@ class HMM(object):
             The row-stochastic transition matrix for which the stationary probabilities are to be computed.
         tol : float, optional, default=1e-5
             The absolute tolerance in total variation distance between probability vector iterates at which iterations are terminated.
-        maxits : int, optional default=None
+        maxits : int, optional, default=None
             If not None, the maximum number of iterations to perform.
+        method : str, optional, default='arpack'
+            Method of stationary eigenvector computation: ['arpack', 'inverse-iteration']
 
         Returns
         -------
@@ -70,40 +72,55 @@ class HMM(object):
         Examples
         --------
         >>> import testsystems
-        >>> Tij = testsystems.transition_matrix()
+        >>> Tij = testsystems.generate_transition_matrix(nstates=3, reversible=True)
         >>> Pi = HMM._compute_stationary_probabilities(Tij)
 
         """
         nstates = Tij.shape[0]
 
-        T = np.array(Tij, dtype=np.float64)
-        mu = 1.0 # eigenvalue corresponding to eigenvector to extract
-        I = np.eye(nstates, dtype=np.float64)
-        b_old = np.ones([nstates], dtype=np.float64) / float(nstates) # initial guess
+        if method == 'arpack':
+            # Compute stationary probability using ARPACK.
+            # TODO: Pass 'maxits' and 'tol' to ARPACK?
+            from scipy.sparse.linalg import eigs
+            from numpy.linalg import norm
+            [eigenvalues, eigenvectors] = eigs(Tij.T, k=1, which='LR')
+            eigenvectors = np.real(eigenvectors)
+            Pi = eigenvectors[:,0] / eigenvectors[:,0].sum()
+            return Pi
+        elif method == 'inverse-iteration':
+            T = np.array(Tij, dtype=np.float64) # Promote matrix to float64
+            mu = 1.0 # eigenvalue corresponding to eigenvector to extract
+            I = np.eye(nstates, dtype=np.float64) # identity matrix
+            b_old = np.ones([nstates], dtype=np.float64) / float(nstates) # initial eigenvector guess
 
-        # Perform inverse iteration.
-        converged = False
-        iteration = 1
-        while not converged:
-            # Update eigenvector guess
-            b_new = np.dot(np.linalg.inv(T - mu*I), b_old)
+            # Perform inverse iteration.
+            converged = False
+            iteration = 1
+            while not converged:
+                # Update eigenvector guess
+                b_new = np.dot(np.linalg.inv((T - mu*I).T), b_old)
 
-            # Normalize to be a probability.
-            b_new /= b_new.sum()
+                # Normalize to be a probability.
+                b_new /= b_new.sum()
 
-            # Compute total variation probability difference.
-            delta = 0.5 * np.absolute(b_new - b_old).sum()
+                # Compute total variation probability difference.
+                delta = 0.5 * np.absolute(b_new - b_old).sum()
 
-            if maxits:
-                converged = (iteration >= maxits)
-            if tol:
-                converged = (delta < tol)
+                # Check convergence criterion.
+                if maxits:
+                    converged = (iteration >= maxits)
+                if tol:
+                    converged = (delta < tol)
 
-            iteration += 1
-            b_old = b_new
+                # Proceed to next iteration.
+                if not converged:
+                    iteration += 1
+                    b_old = b_new
 
-        # Normalize vector to sum to unity.
-        b_new = b_new / b_new.sum()
+            # Normalize vector to sum to unity.
+            Pi = b_new / b_new.sum()
+            return Pi
+        else:
+            raise Exception("method %s unknown." % method)
 
-        return b_new
-
+        return

bhmm/tests/test_hmm.py
@@ -6,18 +6,20 @@ Test HMM functions.
 """
 
 import numpy as np
-import numpy.linalg as linalg
+import scipy.sparse.linalg
 
-from nose.tools import ok_, eq_
+from bhmm import testsystems
+
+from nose.tools import assert_equal, assert_almost_equal
+from numpy.testing import assert_array_almost_equal
 
 def test_hmm():
     # Create a simple HMM model.
-    from bhmm import testsystems
     model = testsystems.three_state_model()
     # Test model parameter access.
-    eq_(model.Tij.shape, (3,3))
-    eq_(model.Pi.shape, (3,))
-    eq_(model.logPi.shape, (3,))
+    assert_equal(model.Tij.shape, (3,3))
+    assert_equal(model.Pi.shape, (3,))
+    assert_equal(model.logPi.shape, (3,))
 
     return
 
@@ -27,12 +29,17 @@ def test_two_state_model():
     from bhmm import hmm
     # Create a simple two-state model.
     nstates = 2
-    Tij = np.array([[0.8, 0.2], [0.5, 0.5]], np.float64)
-    Pi = np.array([0.5, 0.5], np.float64)
+    Tij = testsystems.generate_transition_matrix(reversible=True)
     states = [ {'mu' : -1, 'sigma' : 1}, {'mu' : +1, 'sigma' : 1} ]
     model = hmm.HMM(nstates, Tij, states)
+    # Compute stationary probability using ARPACK.
+    from scipy.sparse.linalg import eigs
+    from numpy.linalg import norm
+    [eigenvalues, eigenvectors] = eigs(Tij.T, k=1, which='LR')
+    eigenvectors = np.real(eigenvectors)
+    Pi = eigenvectors[:,0] / eigenvectors[:,0].sum()
     # Test model is correct.
-    eq_(linalg.norm(model.Tij - Tij), 0.0)
-    eq_(model.states, states)
-    eq_(linalg.norm(model.Pi - Pi), 0.0)
+    assert_array_almost_equal(model.Tij, Tij)
+    assert_equal(model.states, states)
+    assert_array_almost_equal(model.Pi, Pi)
 

bhmm/testsystems.py
@@ -8,24 +8,47 @@ from scipy import linalg
 
 from hmm import HMM
 
-def transition_matrix(nstates=3):
+def generate_transition_matrix(nstates=3, reversible=True):
     """
-    Construct a test transition matrix.
+    Construct test transition matrices.
+
+    Parameters
+    ----------
+    nstates : int, optional, default=3
+        Number of states for which row-stockastic transition matrix is to be generated.
+    reversible : bool, optional, default=True
+        If True, the row-stochastic transition matrix will be reversible.
 
     Returns
     -------
     Tij : np.array with shape (nstates, nstates)
-        The transition matrix.
+        A randomly generated row-stochastic transition matrix.
+
+    TODO
+    ----
+    * Ensure matrices are metastable such that Tii > 0.5.
 
     """
-    if nstates != 3:
-        raise Exception("Only 3 states are supported right now.")
 
-    # Define row-stochastic rate matrix that satisfies detailed balance, and compute transition matrix from this.
-    Kij = np.array([[-0.10,  0.10,  0.00],
-                     [ 0.10, -0.15,  0.05],
-                     [ 0.00,  0.05, -0.05]], np.float64)
-    Tij = linalg.expm(Kij);
+    X = np.random.random([nstates,nstates]) # generate random matrix
+    if reversible:
+        Cij = (X + X.T) / 2.0 # generate symmetric matrix
+    else:
+        Cij = X # asymmetric matrix
+
+    # Compute row-stochastic transition matrix.
+    Tij = Cij
+    for i in range(nstates):
+        Tij[i,:] /= Tij[i,:].sum()
+
+#    if nstates != 3:
+#        raise Exception("Only 3 states are supported right now.")
+#
+#    # Define row-stochastic rate matrix that satisfies detailed balance, and compute transition matrix from this.
+#    Kij = np.array([[-0.10,  0.10,  0.00],
+#                     [ 0.10, -0.15,  0.05],
+#                     [ 0.00,  0.05, -0.05]], np.float64)
+#    Tij = linalg.expm(Kij);
 
     return Tij
 
@@ -47,7 +70,7 @@ def three_state_model(sigma=1.0):
     states.append({ 'mu' :  0, 'sigma' : sigma })
     states.append({ 'mu' : +1, 'sigma' : sigma })
 
-    Tij = transition_matrix(nstates)
+    Tij = generate_transition_matrix(nstates, reversible=True)
 
     # Construct HMM with these parameters.
     model = HMM(nstates, Tij, states)