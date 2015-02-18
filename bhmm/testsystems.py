76da3cb7faa06389ef28ca5f0fda8d40db95939c
noe <frank.noe@fu-berlin.de>
2015-02-18 19:11:21 +0100

    new test system

--- bhmm/testsystems.py
+++ bhmm/testsystems.py
@@ -51,59 +51,43 @@ def generate_transition_matrix(nstates=3, lifetime_max=100, lifetime_min=10, rev
     return T
 
 
-def regular_model(nstates = 3, sigma=1.0, omin = -1, omax = 1, lifetime_max = 10000, lifetime_min = 100, reversible = True):
+def dalton_model(nstates = 3, omin = -1, omax = 1, sigma_min = 0.5, sigma_max = 2.0, lifetime_max = 10000, lifetime_min = 100, reversible = True):
     """
-    Construct a test two-state model with variable emission width.
+    Construct a test two-state model with regular spaced emission means (linearly interpolated between omin and omax)
+    and variable emission widths (linearly interpolated between sigma_min and sigma_max).
 
     Parameters
     ----------
-    sigma : float
-        The width of the observed gaussian distribution for each state.
+    nstates : int, optional, default = 3
+        number of hidden states
+    omin : float, optional, default = -1
+        mean position of the first state.
+    omax : float, optional, default = 1
+        mean position of the last state.
+    sigma_min : float, optional, default = 0.5
+        The width of the observed gaussian distribution for the first state
+    sigma_max : float, optional, default = 2.0
+        The width of the observed gaussian distribution for the last state
+    lifetime_max : float, optional, default = 100
+        maximum lifetime of any state
+    lifetime_min : float, optional, default = 10
+        minimum lifetime of any state
+    reversible : bool, optional, default=True
+        If True, the row-stochastic transition matrix will be reversible.
 
     """
     nstates = 3
 
     # parameters
     means = np.linspace(omin, omax, num = nstates)
+    sigmas = np.linspace(sigma_min, sigma_max, num = nstates)
 
     # Define state emission probabilities.
     states = list()
     for i in range(nstates):
-        states.append({ 'model' : 'gaussian', 'mu' : means[i], 'sigma' : sigma })
+        states.append({ 'model' : 'gaussian', 'mu' : means[i], 'sigma' : sigmas[i] })
 
     Tij = generate_transition_matrix(nstates, lifetime_max = lifetime_max, lifetime_min = lifetime_min, reversible = reversible)
-
-    # Construct HMM with these parameters.
-    from bhmm import HMM
-    model = HMM(nstates, Tij, states)
-
-    return model
-
-
-def generate_random_model(nstates, lifetime_max = 10000, lifetime_min = 100, reversible=True):
-    """Generate a random HMM model with the specified number of states.
-
-    Parameters
-    ----------
-    nstates : int
-        The number of states for the model.
-    reversible : bool, optional, default=True
-        If True, the row-stochastic transition matrix will be reversible.
-
-    Returns
-    -------
-    model : HMM
-        The randomly generated HMM model.
-
-    """
-    Tij = generate_transition_matrix(nstates, lifetime_max = lifetime_max, lifetime_min = lifetime_min, reversible = reversible)
-
-    states = list()
-    for index in range(nstates):
-        # TODO: Come up with a better prior with tunable hyperparameters on mu and sigma.
-        mu = np.random.randn()
-        sigma = np.random.random()
-        states.append({ 'model' : 'gaussian', 'mu' : mu, 'sigma' : sigma })
 
     # Construct HMM with these parameters.
     from bhmm import HMM
@@ -144,7 +128,7 @@ def generate_random_bhmm(nstates=3, ntrajectories=10, length=100,
     """
 
     # Generate a random HMM model.
-    model = generate_random_model(nstates, lifetime_max = lifetime_max, lifetime_min = lifetime_min, reversible = reversible)
+    model = dalton_model(nstates, lifetime_max = lifetime_max, lifetime_min = lifetime_min, reversible = reversible)
     # Generate synthetic data.
     observations = model.generate_synthetic_observation_trajectories(ntrajectories=ntrajectories, length=length)
     # Initialize a new BHMM model.
