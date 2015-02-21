c0c90bd59a401d129ddd896ab18517e22d46c858
John Chodera (MSKCC) <choderaj@mskcc.org>
2015-02-21 11:01:47 +0100

    Added some utility functions.

---
+++ bhmm/analysis.py
@@ -0,0 +1,87 @@
+"""
+Analysis of BHMM data.
+
+"""
+
+import numpy as np
+
+def compute_confidence_intervals():
+    """
+    Compute confidence intervals for various properties.
+
+    """
+    pass
+
+def beta_confidence_intervals(ci_X, ntrials, ci=0.95):
+    """
+    Compute confidence intervals of beta distributions.
+
+    Parameters
+    ----------
+    ci_X : numpy.array
+        Computed confidence interval estimate from `ntrials` experiments
+    ntrials : int
+        The number of trials that were run.
+    ci : float, optional, default=0.95
+        Confidence interval to report (e.g. 0.95 for 95% confidence interval)
+
+    Returns
+    -------
+    Plow : float
+        The lower bound of the symmetric confidence interval.
+    Phigh : float
+        The upper bound of the symmetric confidence interval.
+
+    """
+    # Compute low and high confidence interval for symmetric CI about mean.
+    ci_low = 0.5 - ci/2;
+    ci_high = 0.5 + ci/2;
+
+    # Compute for every element of ci_X.
+    from scipy.stats import beta
+    Plow = ci_X * 0.0;
+    Phigh = ci_X * 0.0;
+    for i in range(ci_X.shape[0]):
+        for j in range(ci_X.shape[1]):
+            Plow[i,j] = beta.ppf(ci_low, a = ci_X[i,j] * ntrials, b = (1-ci_X[i,j]) * ntrials);
+            Phigh[i,j] = beta.ppf(ci_high, a = ci_X[i,j] * ntrials, b = (1-ci_X[i,j]) * ntrials);
+
+return [Plow, Phigh]
+
+def empirical_confidence_interval(sample, interval=0.95):
+    """
+    Compute specified symmetric confidence interval for empirical sample.
+
+    Parameters
+    ----------
+    sample : numpy.array
+        The empirical samples.
+    interval : float, optional, default=0.95
+        Size of desired symmetric confidence interval (0 < interval < 1)
+        e.g. 0.68 for 68% confidence interval, 0.95 for 95% confidence interval
+
+
+    Returns
+    -------
+    low : float
+        The lower bound of the symmetric confidence interval.
+    high : float
+        The upper bound of the symmetric confidence interval.
+    """
+
+    # Sort sample in increasing order.
+    sample = np.sorted(sample)
+
+    # Determine sample size.
+    N = len(sample)
+
+    # Compute low and high indices.
+    low_index = int(np.round((N-1) * (0.5 - interval/2))) + 1
+    high_index = int(np.round((N-1) * (0.5 + interval/2))) + 1
+
+    # Compute low and high.
+    low = sample[low_index]
+    high = sample[high_index]
+
+    return [low, high]
+
