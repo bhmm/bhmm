fixed discrete HMM convergence problem (normalization error)

bhmm/init/discrete.py
@@ -62,13 +62,13 @@ def initial_model_discrete(observations, nstates, lag = 1, reversible = True):
     # expand B_conn to full state space
     B[:,giant] = B_conn[:,:]
 
-    # DE-MIX populations: Assign all output probability to the largest contributor.
-    # This step is a bit made-up because (A,B) is now not a self-consistent PCCA pair anymore - however it works much better.
-    sums = B.sum(axis=0)
-    amax = B.argmax(axis=0)
-    B[:,:] = eps
-    for i in range(B.shape[1]):
-        B[amax[i],i] = sums[i]
+    # # DE-MIX populations: Assign all output probability to the largest contributor.
+    # # This step is a bit made-up because (A,B) is now not a self-consistent PCCA pair anymore - however it works much better.
+    # sums = B.sum(axis=0)
+    # amax = B.argmax(axis=0)
+    # B[:,:] = eps
+    # for i in range(B.shape[1]):
+    #     B[amax[i],i] = sums[i]
     # renormalize B to make it row-stochastic
     B /= B.sum(axis=1)[:,None]
 

bhmm/mlhmm.py
@@ -172,10 +172,8 @@ class MLHMM(object):
         K = len(self.observations)
         N = self.nstates
 
-        #C = np.zeros((N,N))
+        C = np.zeros((N,N))
         gamma0_sum = np.zeros((N))
-        C = np.ones((N,N), dtype=np.float64)
-        #gamma0_sum = np.ones((N), dtype=np.float64)
         for k in range(K):
             # update state counts
             gamma0_sum += gammas[k][0]
@@ -304,6 +302,7 @@ class MLHMM(object):
 
             if it > 0:
                 if loglik - self.likelihoods[it-1] < self.accuracy:
+                    print "CONVERGED! Likelihood change = ",(loglik - self.likelihoods[it-1])
                     converged = True
 
             it += 1