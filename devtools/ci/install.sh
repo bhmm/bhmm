3982823a7667e5cde12ab02234ef534040f6172a
John Chodera (MSKCC) <choderaj@mskcc.org>
2015-03-22 21:08:23 -0400

    Make conda installs quiet.

--- devtools/ci/install.sh
+++ devtools/ci/install.sh
@@ -14,7 +14,7 @@ export PATH=$HOME/miniconda/bin:$PATH
 sudo apt-get update
 sudo apt-get install -qq -y g++ gfortran csh g++-multilib gcc-multilib openbabel
 
-conda update --yes conda
+conda update --yes --quiet conda
 conda config --add channels http://conda.binstar.org/omnia
 conda config --add channels https://conda.binstar.org/rdkit
-conda install --yes conda-build jinja2 binstar pip
+conda install --yes --quiet conda-build jinja2 binstar pip
