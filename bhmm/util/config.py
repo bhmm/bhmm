27b7d8f6ebf95a82a401f3d02b44439133b6f397
noe <frank.noe@fu-berlin.de>
2015-05-02 18:56:28 +0200

    refactored example scripts

--- bhmm/util/config.py
+++ bhmm/util/config.py
@@ -6,9 +6,20 @@ Default configurations
 __author__ = 'noe'
 
 import numpy as np
+import logging
 
 # compute kernel. Could be 'c' or 'python'
 kernel = 'c'
 
 # data type for floating-point operations. Use np.float32 or np.float64
 dtype = np.float64
+
+# print a lot of info?
+verbose = True
+
+
+def log_level():
+    if verbose:
+        return logging.INFO
+    else:
+        return logging.DEBUG
--- bhmm/util/logger.py
+++ bhmm/util/logger.py
@@ -1,7 +1,6 @@
 import logging
 import sys
-
-log_level = logging.DEBUG
+from bhmm.util import config
 
 def logger(name='BHMM', pattern='%(asctime)s %(levelname)s %(name)s: %(message)s',
            date_format='%H:%M:%S', handler=logging.StreamHandler(sys.stdout)):
@@ -21,10 +20,10 @@ def logger(name='BHMM', pattern='%(asctime)s %(levelname)s %(name)s: %(message)s
     :rtype: Logger
     """
     _logger = logging.getLogger(name)
-    _logger.setLevel(log_level)
+    _logger.setLevel(config.log_level())
     if not _logger.handlers:
         formatter = logging.Formatter(pattern, date_format)
         handler.setFormatter(formatter)
-        handler.setLevel(log_level)
+        handler.setLevel(config.log_level())
         _logger.addHandler(handler)
     return _logger
