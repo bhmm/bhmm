fa6e7b2e86bdeeaa999c95fe0e489da0341e16a7
John Chodera (MSKCC) <choderaj@mskcc.org>
2015-07-09 09:25:30 -0400

    Updated setup.py to 0.4.2

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ from Cython.Build import cythonize
 DOCLINES = __doc__.split("\n")
 
 ########################
-VERSION = "0.4.1"
+VERSION = "0.4.2"
 ISRELEASED = False
 __version__ = VERSION
 ########################
