cf55f590071abb6b5f08566187b5a027ad124434
John Chodera (MSKCC) <choderaj@mskcc.org>
2015-09-18 12:14:03 +0200

    Synchronize dev conda recipe with setup.py and real conda recipe.

--- devtools/conda-recipe/build.sh
+++ devtools/conda-recipe/build.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-cp -r $RECIPE_DIR/../.. $SRC_DIR
-$PYTHON setup.py clean
 $PYTHON setup.py install
-
-# Push examples to anaconda/share/bhmm/examples/
-#mkdir $PREFIX/share/bhmm
-#cp -r $RECIPE_DIR/../../examples $PREFIX/share/bhmm/
