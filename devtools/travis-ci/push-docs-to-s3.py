10bea1c2ca04dc73e26a38ab7a90c6d3d74d43a1
John Chodera (MSKCC) <choderaj@mskcc.org>
2016-03-28 23:47:24 -0400

    Update travis-ci

--- devtools/travis-ci/push-docs-to-s3.py
+++ devtools/travis-ci/push-docs-to-s3.py
@@ -1,15 +1,24 @@
+#!/usr/bin/env python
+
+"""
+Must have the vollowing environment variables defined:
+* BUCKET_NAME : AWS bucket name
+* PREFIX : 'latest' or other version number
+
+"""
+
 import os
 import pip
 import tempfile
 import subprocess
-import bhmm.version
+import thermopyl.version
 
 
-BUCKET_NAME = 'bhmm'
-if not bhmm.version.release:
+BUCKET_NAME = 'thermopyl.org'
+if not thermopyl.version.release:
     PREFIX = 'latest'
 else:
-    PREFIX = bhmm.version.short_version
+    PREFIX = thermopyl.version.short_version
 
 if not any(d.project_name == 's3cmd' for d in pip.get_installed_distributions()):
     raise ImportError('The s3cmd pacakge is required. try $ pip install s3cmd')
@@ -22,8 +31,8 @@ secret_key = {AWS_SECRET_ACCESS_KEY}
 '''.format(**os.environ))
     f.flush()
 
-    template = ('s3cmd --config {config} '
-                'sync docs/_build/html/ s3://{bucket}/{prefix}/')
+    template = ('s3cmd --guess-mime-type --config {config} '
+                'sync docs/_build/ s3://{bucket}/{prefix}/')
     cmd = template.format(
             config=f.name,
             bucket=BUCKET_NAME,
@@ -31,10 +40,9 @@ secret_key = {AWS_SECRET_ACCESS_KEY}
     return_val = subprocess.call(cmd.split())
 
     # Sync index file.
-    template = ('s3cmd --config {config} '
+    template = ('s3cmd --guess-mime-type --config {config} '
                 'sync devtools/ci/index.html s3://{bucket}/')
     cmd = template.format(
             config=f.name,
             bucket=BUCKET_NAME)
     return_val = subprocess.call(cmd.split())
-
